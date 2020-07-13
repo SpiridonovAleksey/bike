6b09b668940c763f76f189de660b6b9d37a80c6d
Алексей <televizor5259@gmail.com>
2020-07-13 14:48:36 +0300

    Добавляет подвал

--- build/js/script.js
+++ build/js/script.js
@@ -1,28 +1,18 @@
 'use strict';
-var mainNav = document.querySelector(".main-nav");
-var navButton = document.querySelector(".nav-button");
+var mainNav = document.querySelector('.main-nav');
+var navButton = document.querySelector('.nav-button');
 
 
-mainNav.classList.add("main-nav--none");
-navButton.classList.add("nav-button--close");
+mainNav.classList.add('main-nav--none');
+navButton.classList.add('nav-button--close');
 
 
-navButton.addEventListener("click", function() {
-  if (mainNav.classList.contains("main-nav--none")) {
-    mainNav.classList.remove("main-nav--none");
-    navButton.classList.add("nav-button--open");
+navButton.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-nav--none')) {
+    mainNav.classList.remove('main-nav--none');
+    navButton.classList.add('nav-button--open');
   } else {
-    mainNav.classList.add("main-nav--none");
-    navButton.classList.remove("nav-button--open");
+    mainNav.classList.add('main-nav--none');
+    navButton.classList.remove('nav-button--open');
   }
 });
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
